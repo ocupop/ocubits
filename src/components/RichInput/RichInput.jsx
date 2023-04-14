4173ccf2554a56227babb4069ebba0ab601969e0
PAUL STROOT <paul@ocupop.com>
2023-04-14 10:21:28 -0500

    Add RichInput component

---
+++ src/components/RichInput/RichInput.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+import { getIn } from 'formik'
+
+import Label from '../Label/Label'
+import './RichInput.css'
+// ----------------------------------------------------------------------
+
+RichInput.propTypes = {
+  className: PropTypes.string,
+  theme: PropTypes.string,
+  innerRef: PropTypes.func,
+  hint: PropTypes.string,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  required: PropTypes.bool,
+  field: PropTypes.instanceOf(Object),
+  form: PropTypes.instanceOf(Object),
+  onChange: PropTypes.func
+}
+
+RichInput.defaultProps = {
+  className: '',
+  theme: 'snow',
+  required: false,
+  placeholder: ''
+}
+
+export default function RichInput ({
+  className,
+  innerRef,
+  theme,
+  hint,
+  label,
+  placeholder,
+  required,
+  field,
+  form,
+  form: { errors, touched },
+  onChange
+}) {
+  const status = getIn(touched, field.name) && getIn(errors, field.name) ? 'invalid' : ''
+  const doOnChange = (val) => {
+    if (onChange) onChange(val)
+    form.setFieldValue(field.name, val)
+  }
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ list: 'ordered' }, { list: 'bullet' }, { indent: '-1' }, { indent: '+1' }],
+      ['link', 'image'],
+      ['clean']
+    ]
+  }
+
+  return (
+    <div className={`ocu-richinput form-group ${className}`}>
+      <Label label={label} hint={hint} htmlFor={field.name} required={required}/>
+      <ReactQuill
+        // id="applied_to_quill"
+        // defaultValue - handled by initialValues
+        readOnly = {false}
+        modules = {modules}
+        // formats={formats}
+
+        // style
+        // tabIndex
+        // bounds
+        // children
+        // onChangeSelection
+        // onBlur/onFocus/onBlur/onKeyPress/onkeyDown/onKeyUp
+        // perserveWhitespace
+
+        className={`form-input  ${status}`}
+        {...field}
+        theme={theme}
+        value={field.value}
+        onChange={doOnChange}
+        required={required}
+        placeholder={placeholder}
+        onBlur={(range, source, quill) => {
+          /* Do Nothing */
+        }}
+      />
+      {getIn(touched, field.name) && getIn(errors, field.name) && (
+        <small className="form-validation-error">{getIn(errors, field.name)}</small>
+      )}
+    </div>
+  )
+}
