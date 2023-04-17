dd0ad8c0f19b9b27a8d04d21273efa65ca361fc5
PAUL STROOT <paul@ocupop.com>
2023-04-17 11:29:31 -0500

    updates to DateInput

---
+++ src/components/DateInput/DateInput.jsx
@@ -0,0 +1,120 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { getIn } from 'formik'
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
+
+
+import Label from '../Label/Label'
+import './DateInput.css'
+// ----------------------------------------------------------------------
+
+DateInput.propTypes = {
+  className: PropTypes.string,
+  innerRef: PropTypes.func,
+  hint: PropTypes.string,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  required: PropTypes.bool,
+  field: PropTypes.instanceOf(Object),
+  form: PropTypes.instanceOf(Object),
+  range: PropTypes.bool,
+  twoFields: PropTypes.bool,
+  isClearable: PropTypes.bool,
+  onChange: PropTypes.func
+}
+
+DateInput.defaultProps = {
+  className: '',
+  required: false,
+  placeholder: '',
+  range: false,
+  twoFields: true,
+  isClearable: false,
+  onChange: () => null
+}
+
+export default function DateInput ({
+  className,
+  innerRef,
+  hint,
+  label,
+  placeholder,
+  required,
+  field,
+  form,
+  form: { errors, touched },
+  onChange,
+  isClearable,
+  range,
+  twoFields
+}) {
+  const handleValueChange = (newValue) => {
+    console.log(newValue)
+    form.setFieldValue(field.name, newValue)
+    // onChange(newValue)
+  }
+  const setStartDate = (newValue) => {
+    form.setFieldValue(field.name, [newValue,  Array.isArray(field.value) ? field.value[1] : null])
+  }
+  const setEndDate = (newValue) => {
+    form.setFieldValue(field.name, [Array.isArray(field.value) ? field.value[0] : null, newValue])
+  }
+  const status = getIn(touched, field.name) && getIn(errors, field.name) ? 'invalid' : ''
+  const startDate = Array.isArray(field.value) && field.value.length > 0 ? field.value[0] : null
+  const endDate = Array.isArray(field.value) && field.value.length > 1 ? field.value[1] : null
+  return (
+    <div className={`ocu-dateinput form-group ${className}`}>
+      <Label label={label} hint={hint} htmlFor={field.name} required={required}/>
+      <DatePicker
+        className={`form-input ${status}`}
+        selected={(!range && field.value) ? field.value : startDate || null }
+        onChange={(!range || !twoFields) ? handleValueChange : setStartDate}
+        showTimeSelect={false}
+        showIcon={true}
+        dateFormat="MMMM d, yyyy"
+        placeholderText={placeholder}
+        withPortal={false}
+        isClearable={isClearable}
+
+        selectsStart={range && twoFields}
+        selectsRange={range && !twoFields}
+        startDate={range && startDate}
+        endDate={range && endDate}
+
+        // onCalendarClose={handleCalendarClose}
+        // onCalendarOpen={handleCalendarOpen}
+        // timeClassName={handleColor}
+        // popperClassName
+        // popperPlacement="top-end"
+        // popperModifiers
+      />
+
+      {(range && twoFields) &&
+      <DatePicker
+        className={`form-input ${status}`}
+        selected={
+          !Array.isArray(field.value)
+          ? null
+          : field.value.length > 1 ? field.value[1] : field.value.length > 0 ? field.value[0] : null
+        }
+        onChange={setEndDate}
+        showTimeSelect={false}
+        showIcon={true}
+        dateFormat="MMMM d, yyyy"
+        isClearable={isClearable}
+        placeholderText={placeholder}
+        withPortal={false}
+
+        selectsEnd
+        startDate={startDate}
+        endDate={endDate}
+      />
+      }
+
+      {getIn(touched, field.name) && getIn(errors, field.name) && (
+        <small className="form-validation-error">{getIn(errors, field.name)}</small>
+      )}
+    </div>
+  )
+}
---
+++ src/components/DateInput/DateInput.stories.jsx
@@ -0,0 +1,57 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import { Field } from 'formik'
+
+import { FormikWrapper, withCenteredStory } from '@lib'
+import DateInput from './DateInput'
+// ----------------------------------------------------------------------
+
+export default {
+  title: 'Fields/DateInput',
+  component: DateInput,
+  decorators: [withCenteredStory],
+  parameters: {
+    controls: { include: ['name', 'label', 'hint', 'placeholder', 'required', 'initialValues', 'range','twoFields','isClearable','debug'] }
+  },
+  argTypes: {
+    range: { control: 'boolean' },
+    twoFields: { control: 'boolean', if: { arg: 'range', truthy: true } },
+  },
+  args: {
+    name: 'date',
+    label: 'Select a Date',
+    hint: '',
+    placeholder: '',
+    className: '',
+    required: false,
+    initialValues: { },
+    range: false,
+    twoFields: false,
+    isClearable: false,
+    debug: false,
+  }
+}
+
+function Template ({ name, label, hint, placeholder, className, initialValues, required, range, twoFields, isClearable, debug }) {
+  return (
+    <FormikWrapper
+      initialValues={initialValues}
+      debug={debug}
+    >
+      <Field
+        component={DateInput}
+        name={name}
+        label={label}
+        hint={hint}
+        placeholder={placeholder}
+        required={required}
+        className={className}
+        range={range}
+        twoFields={twoFields}
+        isClearable={isClearable}
+      />
+    </FormikWrapper>
+  )
+}
+
+export const Basic = Template.bind({})
