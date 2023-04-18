99b415233315a172724ef1b9bd2d5eaa4ea403ef
PAUL STROOT <paul@ocupop.com>
2023-04-18 16:31:20 -0500

    Add Draft JS rich input component

---
+++ src/components/DraftJS/RichInput.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { EditorState, setEditorState } from 'draft-js'
+import { Editor } from 'react-draft-wysiwyg'
+import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+import { getIn } from 'formik'
+
+import Label from '../Label/Label'
+import './RichInput.css'
+// ----------------------------------------------------------------------
+
+RichInput.propTypes = {
+  className: PropTypes.string,
+  theme: PropTypes.string,
+  hint: PropTypes.string,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  required: PropTypes.bool,
+  field: PropTypes.instanceOf(Object),
+  form: PropTypes.instanceOf(Object),
+  onChange: PropTypes.func
+}
+
+RichInput.defaultProps = {
+  className: '',
+  theme: 'snow',
+  required: false,
+  placeholder: ''
+}
+
+export default function RichInput ({
+  className,
+  hint,
+  label,
+  required,
+  placeholder,
+  field,
+  form,
+  form: { errors, touched },
+  onChange
+}) {
+  const status = getIn(touched, field.name) && getIn(errors, field.name) ? 'invalid' : ''
+  const doOnChange = (val) => {
+    console.log(val)
+    if (onChange) onChange(val)
+    form.setFieldValue(field.name, val)
+  }
+
+  return (
+    <div className={`ocu-richinput form-group ${className} ${status}`}>
+      <Label label={label} hint={hint} htmlFor={field.name} required={required}/>
+      <Editor
+        placeholder={placeholder}
+        editorState={field.value || EditorState.createEmpty()}
+        toolbarClassName="toolbar"
+        wrapperClassName="wrapperClassName"
+        editorClassName="editWindow"
+        onEditorStateChange={doOnChange}
+      />
+      {getIn(touched, field.name) && getIn(errors, field.name) && (
+        <small className="form-validation-error">{getIn(errors, field.name)}</small>
+      )}
+    </div>
+  )
+}
