b60faf410c983d97ed98fbe24d07b834cf7257fa
cdnicoll <cdnicoll@gmail.com>
2023-04-24 13:41:50 -0700

    export cleanup

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -1,40 +1,44 @@
-import React, { useState, FC } from 'react'
-import PropTypes, { bool } from 'prop-types'
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import './Tooltip.css'
 
 // ----------------------------------------------------------------------
 
-const ToolTip = ({
+export default function ToolTip ({
   children,
   className,
   placement,
   triggerType,
   unstyled,
   ...props
-}) => {
+}) {
   const [show, setShow] = useState(false)
 
   return (
-    <div {...props} className={`ocu-tooltip ${className}`} data-unstyled={unstyled ? true : null}>
+    <div
+      {...props}
+      className={`ocu-tooltip ${className}`}
+      data-unstyled={unstyled ? true : null}
+    >
       {show && (
         <div
-          id="tooltip-default"
-          role="tooltip"
+          id='tooltip-default'
+          role='tooltip'
           style={{ minWidth: '200px', textAlign: 'center' }}
-          className={`tooltip ${placement}  `}>
+          className={`tooltip ${placement}  `}
+        >
           {children}
-          <div
-            className="tooltip-arrow"
-            data-popper-arrow></div>
+          <div className='tooltip-arrow' data-popper-arrow></div>
         </div>
       )}
       <button
         onMouseOver={() => setShow(true)}
         onMouseOut={() => setShow(false)}
-        data-tooltip-target="tooltip-default"
+        data-tooltip-target='tooltip-default'
         style={{ lineHeight: 0 }}
-        className="">
-        ?<i className="fas fa-question-circle icon"></i>
+        className=''
+      >
+        ?<i className='fas fa-question-circle icon'></i>
       </button>
     </div>
   )
@@ -51,8 +55,5 @@ ToolTip.defaultProps = {
   children: PropTypes.node,
   className: '',
   placement: 'top',
-  // triggerType: hover,
   unstyled: false
 }
-
-export default ToolTip
