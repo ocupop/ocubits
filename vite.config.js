27c8436a3863e9afd673e1d1b1abe6d97fbe3a4f
cdnicoll <cdnicoll@gmail.com>
2023-05-04 07:58:39 -0700

    Updates to the dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,38 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
+import { visualizer } from 'rollup-plugin-visualizer'
+import multi from 'rollup-plugin-multi-entry'
+
+// https://stackoverflow.com/questions/75746767/is-there-any-bundle-analyzer-for-vite
+// https://www.npmjs.com/package/rollup-plugin-visualizer
+const visualizerOptions = {
+  template: 'treemap', // or sunburst
+  open: true,
+  gzipSize: true,
+  brotliSize: true,
+  filename: 'analice.html'
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [dts(), react()],
+  plugins: [dts(), react(), visualizer(visualizerOptions)],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
       name: 'ocubits',
-      formats: ['umd', 'es'],
+      formats: ['es'],
       fileName: 'ocubits'
     },
     rollupOptions: {
-      external: ['react', 'react-dom']
+      plugins: [multi()],
+      external: ['react', 'react-dom'],
+      output: {
+        // manualChunks: () => 'ignored', // Disable default chunking
+        // assetFileNames: 'components/[name][extname]' // Output files to components/ directory
+        dir: 'dist'
+        // format: 'esm'
+      }
     }
   },
   resolve: {
